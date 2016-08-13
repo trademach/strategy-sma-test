56915085317c85a7124d66e634e85869195e6e63
Marco Au <marco.au.321@gmail.com>
2016-08-13 23:13:17 +0800

    update mq logic and unify outgoing mq message format

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const indicators = require('./indicators');
 const INSTRUMENTS = config.get('instruments');
 const SMA_DURATION = config.get('strategy.sma-duration');
 
+const MQ_INFLOW_URI = config.get('mq.inflow.uri');
+const MQ_INFLOW_TOPIC = config.get('mq.inflow.topic');
+
+const MQ_OUTFLOW_URI = config.get('mq.outflow.uri');
+const MQ_OUTFLOW_TOPICS = config.get('mq.outflow.topics');
+
 const socketIn = zmq.socket('sub');
 const socketOut = zmq.socket('pub');
 
@@ -21,11 +27,11 @@ INSTRUMENTS.forEach(i => tickQueues[i] = []);
 let lastDiffs = {};
 
 function init() {
-  socketIn.connect(config.get('mq.inflow.uri'));
-  socketIn.subscribe('oanda');
+  socketIn.connect(MQ_INFLOW_URI);
+  socketIn.subscribe(MQ_INFLOW_TOPIC);
   socketIn.on('message', handleMessage);
 
-  socketOut.connect(config.get('mq.outflow.uri'));
+  socketOut.connect(MQ_OUTFLOW_URI);
 }
 
 function handleMessage(topic, data) {
@@ -82,31 +88,17 @@ function handleMessage(topic, data) {
 
     console.log(`signal - ${signal} => ${instrument}`);
 
-    // send to OANDA executer
-    const executerMessage = {
+    const outflowMessage = {
+      strategy: 'smaTest',
       instrument: instrument,
       action: 'match',
       position: position,
-      type: 'market'
-    };
-    socketOut.send([
-      'executer',
-      JSON.stringify(executerMessage)
-    ]);
-
-    // send to Telegram messager
-    const messagerMessage = {
-      strategy: 'smaTest',
-      instrument: instrument,
-      signal: signal,
-      position: position,
       ask: tick.ask,
-      bid: tick.bid
+      bid: tick.bid,
+      type: 'market',
     };
-    socketOut.send([
-      'messager',
-      JSON.stringify(messagerMessage)
-    ]);
+
+    MQ_OUTFLOW_TOPICS.forEach(t => socketOut.send([t, JSON.stringify(outflowMessage)]));
   }
 }
 
